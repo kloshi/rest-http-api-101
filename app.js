9bb37005e8aeb5cfb37b27e49976d7a91c615766
Andrzej Gbur <andrzej.gbur@yahoo.com>
2018-12-04 12:56:59 +0100

    moved handler functions to separate file: routes.js

--- app.js
+++ app.js
@@ -1,50 +1,22 @@
 const express = require('express');
-const premium = require('./core/premium')
+const bodyParser = require('body-parser');
 const app = express();
 const port = 8080;
-const bodyParser = require('body-parser');
+
+const premium = require('./core/premium')
+const routes = require('./core/routes')
 
 app.use(bodyParser.urlencoded({extended: true}));
 
 // Routes
-app.get('/', function (req, res) {
-  res.send('Please use /v1/quote/car-insurance ...')
-});
-
-app.get('/v1/quote/car-insurance', function (req, res) {
-  // check if there's input coming with the request, if not return an adequate error
-  // check if the input if correct (separate function), if not return an adequate error
-  // can add authentication to prevent too many requests from a single machine
-  // calculate premium and prepare result in JSON format
-  // return result JSON
-
-  // if no params or bad params:
-  // res.statusCode = 400;
-  // res.setHeader('Location', '/');
-  // return res.end()
-  // else
-  // res.statusCode = 200;
-  // res.setHeader('Location', '/');
-  // build response
-  // send response
-  // return res.end()
-
-  console.log(req.url, req.params);
-  // console.log(req.body.user);
-  // console.log(req.body.user.name);
-  // console.log(req.body.user.email);
-
-  // res.setHeader('Content-Type','text/html');
-  // res.write('<html>');
-  // res.write('</html>');
-  // res.end();
-  res.send('Hello, nothing\'s ready yet')
-});
+app.get('/', routes.handleRoot);
+app.get('/v1/quote/car-insurance', routes.handleQuery);
 
 
 // start server
 app.listen(port);
 console.log(`Running the server on port ${port}...`);
+
 
 // tests (some sample functions for use )
 module.exports = {
